8790e56ed07842400e629f066e12ecbedec6ea88
Derky <derky@phpbb.com>
2019-04-30 22:21:59 +0200

    Fix versions constraints for EPV

--- ext.php
+++ ext.php
@@ -22,7 +22,7 @@ class ext extends \phpbb\extension\base
 	public function is_enableable()
 	{
 		$config = $this->container->get('config');
-		return version_compare($config['version'], '3.1.3', '>=');
+		return version_compare($config['version'], '3.2.6', '>=');
 	}
 
 	/**
